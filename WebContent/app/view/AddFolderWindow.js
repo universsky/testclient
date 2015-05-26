780248f48a1b56ad3e2dd2dfd314147261831103
Liang Chen <gnail_nehc@aliyun.com>
2015-05-27 02:40:16 +0800

    Update AddFolderWindow.js

--- WebContent/app/view/AddFolderWindow.js
+++ WebContent/app/view/AddFolderWindow.js
@@ -33,58 +33,31 @@ Ext.define('MyApp.view.AddFolderWindow', {
                             	var form=Ext.getCmp('AddFolderForm').getForm();
                             	if(form.isValid()){
                             		var folder=form.findField('folderName').getValue();
-                            		var children=Ext.getCmp('AddFolderForm').theNode.childNodes;
-                            		for(var i=0;i<children.length;i++){
-                            			if(!children[i].get("leaf")){
-                            				if(children[i].get("text").toLowerCase()==folder.toLowerCase()){
-                                				Ext.Msg.alert("错误",folder+"名称重复，请重新输入");
-                                				return;
-                                			}
-                            			}
-                            		}
-                                	var path=Ext.getCmp('AddFolderForm').theNode.raw.folderName+"/"+folder+"-dir";
-                                	
-                                	
-                                	var childnode={
-                                			id:folder.replace('/','>'),
-                                		    text:folder,
-                                		    leaf:false,
-                                		    folderName:path,
-                                		    root:false,
-                                		    allowDrag: true,
-                                		    allowDrop: true,
-                                		    expandable: false
-                                	};
-                                	Ext.getCmp('AddFolderForm').theNode.insertChild(0,childnode);
-                                	Ext.getCmp('AddFolderForm').theNode.expand();
-                                	me.close();
-                                	
-//                                	thestore.proxy.extraParams={
-//                                    		folderName:path
-//                                    	};
-//                                	thestore.sync({
-//                                		success:function(betch,options){
-//                                		},
-//                                		failure:function(betch,options){
-//                                			Ext.Msg.alert("错误","插入目录失败");
-//                                			thestore.load();
-//                                		}
-//                                	});
-                                	
-                                	Ext.Ajax.request( {  
+                            		var path=Ext.getCmp('AddFolderForm').theNode.raw.folderName+"/"+folder+"-dir";
+                            		Ext.Ajax.request( {  
 										url : 'job/addNode',
 										params : {
 											folderName : path
 										},  
 									    success : function(response, options) {
-//									    	Ext.Ajax.request( {  
-//												url : 'job/subscribleQueue',
-//											    success : function(response, options) {
-//											    },  
-//											    failure: function(response, opts) {
-//									             	console.log("错误","插入队列失败");
-//									            }
-//											});
+									    	var json=JSON.parse(response.responseText);
+									    	if(json.success){
+									    		var childnode={
+		                                			id:folder.replace('/','>'),
+		                                		    text:folder,
+		                                		    leaf:false,
+		                                		    folderName:path,
+		                                		    root:false,
+		                                		    allowDrag: true,
+		                                		    allowDrop: true,
+		                                		    expandable: false
+			                                	};
+			                                	Ext.getCmp('AddFolderForm').theNode.insertChild(0,childnode);
+			                                	Ext.getCmp('AddFolderForm').theNode.expand();
+			                                	me.close();
+									    	}else{
+									    		Ext.Msg.alert("错误",json.msg);
+									    	}
 									    },  
 									    failure: function(response, opts) {
 							             	Ext.Msg.alert("错误","插入目录失败");
