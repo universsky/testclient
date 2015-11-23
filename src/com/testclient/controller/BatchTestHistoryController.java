16b2bb9e2bba16a58a6ea2701b7ace8dc9ad956f
Liang Chen <gnail_nehc@aliyun.com>
2015-11-23 12:37:52 +0800

    Update BatchTestHistoryController.java
    add export to excel feature

--- src/com/testclient/controller/BatchTestHistoryController.java
+++ src/com/testclient/controller/BatchTestHistoryController.java
@@ -1,21 +1,37 @@
-package com.testclient.controller;
+package com.alipics.testassets.testclient.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.testclient.enums.LabFolderName;
-import com.testclient.enums.SeperatorDefinition;
-import com.testclient.httpmodel.BatchRunItem;
-import com.testclient.httpmodel.DataGridJson;
-import com.testclient.httpmodel.Json;
-import com.testclient.service.BatchTestHistoryService;
+import com.alipics.testassets.testclient.httpmodel.BatchTestReport;
+import com.alipics.testassets.testclient.httpmodel.DataGridJson;
+import com.alipics.testassets.testclient.httpmodel.Json;
+import com.alipics.testassets.testclient.service.BatchTestHistoryService;
 
 
 @Controller
@@ -56,4 +72,79 @@ public class BatchTestHistoryController {
 		if(f.exists())
 			f.delete();
 	}
+	
+	@RequestMapping(value="/exportBatchTestToExcel" )
+	@ResponseBody
+	public void exportBatchTestToExcel(HttpServletRequest request, HttpServletResponse response,@RequestParam String testPath,@RequestParam String batchTest) {
+		String filename = "ApiTestReport_"+new SimpleDateFormat("yyyy-MM-dd_HHmmss").format(new Date())+".xls";
+		//创建一个新的Excel  
+		HSSFWorkbook workBook = new HSSFWorkbook();
+		//创建sheet页  
+		HSSFSheet sheet = workBook.createSheet("report");  
+		// 设置列宽    
+		sheet.setColumnWidth(0, 256*35);    
+		sheet.setColumnWidth(1, 256*25);    
+		sheet.setColumnWidth(2, 256*15);
+		sheet.setColumnWidth(3, 256*10); 
+		sheet.setColumnWidth(4, 256*60);    
+		sheet.setColumnWidth(5, 256*100);    
+
+		// 创建标题行 
+		HSSFRow row0 = sheet.createRow(0);    
+		// 设置行高    
+		row0.setHeight((short) 400);
+		String columns="API;测试时间;接口耗时（ms）;结果;请求内容;响应内容";int i=0;
+		for(String column : columns.split(";")){
+			HSSFCell cell = row0.createCell(i++);  
+			cell.setCellValue(new HSSFRichTextString(column));   
+		}
+		testPath+=testPath.endsWith("/")? "BatchTesting/" : "/BatchTesting/"+batchTest;
+		List<BatchTestReport> list=batchTestHistoryService.getBatchTestReport(testPath);
+		i=0;
+		for(BatchTestReport r : list){
+			String result=r.getResult();
+			HSSFRow row = sheet.createRow(++i);
+			row.setHeight((short) 2500);
+			HSSFCell cell = row.createCell(0);  
+			cell.setCellValue(new HSSFRichTextString(r.getName()));   
+			cell = row.createCell(1);  
+			cell.setCellValue(new HSSFRichTextString(r.getTime()));   
+			cell = row.createCell(2);  
+			cell.setCellValue(new HSSFRichTextString(r.getDuration()));   
+			cell = row.createCell(3);  
+			cell.setCellValue(new HSSFRichTextString(result));   
+			cell = row.createCell(4);  
+			cell.setCellValue(new HSSFRichTextString(r.getRequest()));   
+			cell = row.createCell(5);  
+			String res=r.getResponse();
+			if(res.length()>32766){
+				res=res.substring(0, 32767);
+			}
+			cell.setCellValue(new HSSFRichTextString(res));   
+		}
+		ServletOutputStream out=null;
+		try {
+			response.reset();
+			response.setContentType("application/vnd.ms-excel");
+			response.setHeader("Content-Disposition", "attachment;filename=" + filename);  
+			response.setCharacterEncoding("utf-8");
+			out = response.getOutputStream();
+			workBook.write(out);
+			workBook.close();
+			response.flushBuffer();
+			out.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			try {
+				out.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		  
+	}
+	
 }
